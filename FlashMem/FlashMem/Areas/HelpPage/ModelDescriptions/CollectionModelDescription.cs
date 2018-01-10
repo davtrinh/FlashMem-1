469e9d3ecf9868afa590692b070aff69ebd0f446
Ted Bui <tedtbui@gmail.com>
2018-01-09 21:16:43 -0800

    Remove Visual Studio folder FlashMem, will not be using this.

--- FlashMem/FlashMem/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace FlashMem.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- FlashMem/FlashMem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FlashMem.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
